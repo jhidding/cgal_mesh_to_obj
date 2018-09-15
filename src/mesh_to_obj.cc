fe773bd6ba37b9efb729e5d278ae73941adabe7d
Johan Hidding <j.hidding@esciencecenter.nl>
2018-09-15 19:54:05 +0200

    modernize example

--- src/mesh_to_obj.cc
+++ src/mesh_to_obj.cc
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <cstdlib>
+#include <functional>
+#include <map>
 
 #include <CGAL/Surface_mesh_default_triangulation_3.h>
 #include <CGAL/Complex_2_in_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
 #include <CGAL/Implicit_surface_3.h>
-// default triangulation for Surface_mesher
 
-typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
-// c2t3
-typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
-typedef Tr::Geom_traits GT;
-typedef GT::Sphere_3 Sphere_3;
-typedef GT::Point_3 Point_3;
-typedef GT::FT FT;
-typedef FT (*Function)(Point_3);
-typedef CGAL::Implicit_surface_3<GT, Function> Surface_3;
+using Triangulation = CGAL::Surface_mesh_default_triangulation_3;
+using Mesh          = CGAL::Complex_2_in_triangulation_3<Triangulation>;
+using Geometry      = Triangulation::Geom_traits;
+using Sphere        = Geometry::Sphere_3;
+using Point         = Geometry::Point_3;
+using real_t        = Geometry::FT;
+using Function      = std::function<real_t (Point)>;
+using Surface       = CGAL::Implicit_surface_3<Geometry, Function>;
 
-FT sphere_function (Point_3 p)
+Function sphere_function(real_t radius)
 {
-  const FT x2=p.x()*p.x(), y2=p.y()*p.y(), z2=p.z()*p.z();
-  return x2 + y2 + z2 - 1;
+  real_t radius_squared = radius * radius;
+  return [=] (Point const &p) {
+    auto v = p - CGAL::ORIGIN;
+    return v*v - radius_squared;
+  };
 }
 
-void write_to_obj(std::ostream &out, C2t3 &mesh)
+void write_to_obj(std::ostream &out, Mesh &mesh)
 {
-  std::map<C2t3::Vertex_handle, unsigned> vertex_map;
+  std::map<Mesh::Vertex_handle, unsigned> vertex_map;
   unsigned count = 1;
 
   for (auto v  = mesh.vertices_begin();
@@ -43,7 +46,7 @@ void write_to_obj(std::ostream &out, C2t3 &mesh)
             f != mesh.facets_end();
             ++f) {
     out << "f";
-    for (unsigned j = 0; j < 4; ++j) {
+    for (int j = 0; j < 4; ++j) {
       if (j != f->second) {
         out << " " << vertex_map[f->first->vertex(j)];
       }
@@ -52,20 +55,25 @@ void write_to_obj(std::ostream &out, C2t3 &mesh)
   }
 }
 
-int main() {
-  Tr tr;            // 3D-Delaunay triangulation
-  C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
-  // defining the surface
-  Surface_3 surface(sphere_function,             // pointer to function
-                    Sphere_3(CGAL::ORIGIN, 2.)); // bounding sphere
+int main()
+{
+  Triangulation tr;   // 3D-Delaunay triangulation
+  Mesh mesh(tr);      // 2D-complex in 3D-Delaunay triangulation
+                      // defining the surface
+  
+  Surface surface(sphere_function(1.0),          // pointer to function
+                  Sphere(CGAL::ORIGIN, 2.));     // bounding sphere
   // Note that "2." above is the *squared* radius of the bounding sphere!
+
   // defining meshing criteria
-  CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,  // angular bound
-                                                     0.1,  // radius bound
-                                                     0.1); // distance bound
+  CGAL::Surface_mesh_default_criteria_3<Triangulation>
+    criteria(30.,     // angular bound
+             0.1,     // radius bound
+             0.1);    // distance bound
+
   // meshing surface
-  CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
-  write_to_obj(std::cout, c2t3);
+  CGAL::make_surface_mesh(mesh, surface, criteria, CGAL::Non_manifold_tag());
+  write_to_obj(std::cout, mesh);
 
   return EXIT_SUCCESS;
 }
